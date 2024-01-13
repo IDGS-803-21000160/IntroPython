d5aa3059598023a50d71d4baf03329292d7ed20a
Ivan Blancarte <iblancarte583@gmail.com>
2024-01-12 18:49:24 -0600

    IDJBA 12/01/2024 Tuplas

---
+++ tema1-Variables.py
@@ -0,0 +1,24 @@
+#Nombre de variables
+
+'''
+comentarios 
+multi 
+linea
+'''
+
+numero1="23"
+numero2="10"
+nombre="Domingo"
+correo="domingo@gmail.com"
+
+print(numero1+numero2)
+
+print("La suma es:")
+print(int(numero1)+int(numero2))
+
+sueldo="938.284"
+valorDecimal=float(sueldo)
+print(valorDecimal)
+
+edad="283"
+print(len(str(edad)))
---
+++ tema3-Funciones1.py
@@ -0,0 +1,9 @@
+texto="Universidad Tecnologica de Leon"
+
+print(texto.lower())
+print(texto.upper())
+print(texto.title())
+print(texto.find("de"))
+print(texto.count("e"))
+
+textoNuevo =texto.replace("e","3")
